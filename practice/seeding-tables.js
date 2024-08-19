cdf8cdddf3d90c9633a9e42d30b1cc04e487664e
NullAux <gverney@outlook.com>
2024-08-19 15:24:22 +0100

    Began seeding-tables to add one row at a time

---
+++ practice/seeding-tables.js
@@ -0,0 +1,59 @@
+const format = require("pg-format")
+const pg = require("pg")//< Using this instead of suggested structure as this project is full of console.logs
+
+const pool = new pg.Pool({
+    database: 'node_postgres_practice_db' 
+})
+
+//Util functions, using pg-format
+function dropTable() {
+    return pool.query('DROP TABLE IF EXISTS seed_practice_table')
+}
+
+function createTable() {
+    return pool.query('CREATE TABLE seed_practice_table (id SERIAL PRIMARY KEY, name VARCHAR(50))')
+}
+
+//Create and seed the table
+async function seedTable() {
+
+    //...Using arrays
+    await dropTable()
+    await createTable()
+
+    const arrayData = 
+    [
+        ['Alpha'],
+        ['Beta'],
+        ['Gamma'],
+        [{objectKey: 'objectValue'}]
+    ]
+
+    await pool.query(format('INSERT INTO seed_practice_table (name) VALUES %L', arrayData))
+    console.log((await pool.query('SELECT * FROM seed_practice_table')).rows)
+
+    //...Using JSON object
+    await dropTable()
+    await createTable()
+    await pool.query('ALTER TABLE seed_practice_table ADD type VARCHAR(50), ADD jsonObj JSON')
+
+    //Pokemon in form name : type
+    const JSONData = {
+        'Bulbasaur' : 'Grass',
+        'Charmander': 'Fire',
+        'Squirtle': 'Water'
+    }
+
+    const queryString = format("INSERT INTO seed_practice_table (name, type, jsonObj) VALUES (%L, %L, %L)", Object.keys(JSONData)[0], JSONData.Charmander, JSONData)
+    console.log(queryString)
+    await pool.query(queryString)
+    console.log((await pool.query('SELECT * FROM seed_practice_table')).rows)
+
+    //await pool.query("UPDATE seed_practice_table SET type = jsonobj -> 'Bulbasaur' WHERE id = 1")
+    //console.log((await pool.query('SELECT * FROM seed_practice_table')).rows)
+
+
+    pool.end()
+}
+
+seedTable()
